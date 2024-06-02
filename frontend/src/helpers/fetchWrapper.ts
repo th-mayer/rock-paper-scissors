bb6cae2c55198ab002d6cc12a8fb2727154dac2e
th-mayer <theresa.mayer@online.de>
2024-06-02 21:42:06 +0200

    add cors handling

--- frontend/src/helpers/fetchWrapper.ts
+++ frontend/src/helpers/fetchWrapper.ts
@@ -1,9 +1,9 @@
 import { useAuthStore } from "../stores/auth.store";
 
 interface RequestOptions {
-  method: string,
-  headers: Headers,
-  body?: any
+  method: string;
+  headers: Headers;
+  body?: any;
 }
 
 // Wrapper for browser built in fetch() method
@@ -12,41 +12,43 @@ export const fetchWrapper = {
   get: makeRequest("GET"),
   post: makeRequest("POST"),
   put: makeRequest("PUT"),
-  delete: makeRequest("DELETE")
-}
+  delete: makeRequest("DELETE"),
+};
 
-function makeRequest(requestType: string){
+function makeRequest(requestType: string) {
   return (url: string, body?: any) => {
     const requestOptions: RequestOptions = {
       method: requestType,
       headers: authHeader(url),
     };
-    if(body) {
+    if (body) {
       requestOptions.headers.append("Content-Type", "application/json");
       requestOptions.body = JSON.stringify(body);
     }
     return fetch(url, requestOptions).then(handleResponse);
-  }
+  };
 }
 
-function authHeader(url: string): Headers{
+function authHeader(url: string): Headers {
   const { user } = useAuthStore();
   const isLoggedIn = !!user?.token;
   const isApiUrl = url.startsWith(import.meta.env.VITE_API_URL);
   const headers = new Headers();
-  if(isLoggedIn && isApiUrl){ 
+  if (isLoggedIn && isApiUrl) {
     headers.append("Authorization", `Bearer ${user.token}`);
   }
   return headers;
 }
 
-async function handleResponse(response:Response) {
-  const isJson = response.headers?.get("content-type")?.includes("application/json");
+async function handleResponse(response: Response) {
+  const isJson = response.headers
+    ?.get("content-type")
+    ?.includes("application/json");
   const data = isJson ? await response.json() : null;
 
-  if(!response.ok){
+  if (!response.ok) {
     const { user, logout } = useAuthStore();
-    if([401, 403].includes(response.status) && user){
+    if ([401, 403].includes(response.status) && user) {
       logout();
     }
     const error = (data && data.message) || response.status;
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,17 @@
 import express, { json } from "express";
 import { createServer } from "http";
+import cors, { CorsOptions } from "cors";
 import userController from "./src/controllers/user.controller";
 import SocketServer from "./src/socket/events";
 import { errorHandler } from "./src/express-middleware/error.handler";
 
-const port = 3000;
+const port = process.env.PORT;
 const app = express();
+const corsOptions : CorsOptions = {
+  origin: process.env.CLIENT_URL
+}
 
+app.use(cors(corsOptions));
 app.use(json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandler);
