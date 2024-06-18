53ff7ba91d12cb9ebde470a6d2706ecfc2f691db
th-mayer <theresa.mayer@online.de>
2024-06-18 10:20:15 +0200

    rebuild userStore with compositionAPI syntax

--- frontend/src/stores/users.store.ts
+++ frontend/src/stores/users.store.ts
@@ -1,62 +1,48 @@
 import { defineStore } from "pinia";
 import { fetchWrapper } from "../helpers/fetchWrapper";
 import { useAuthStore } from "./auth.store";
+import { ref } from "vue";
 
 const baseURL = `${import.meta.env.VITE_API_URL}/users`;
 
-export const useUserStore = defineStore("users", {
-  state: () => ({
-    allUsers: {} as any,
-    user: {} as any,
-  }),
-  actions: {
-    async register(user: any) {
-      await fetchWrapper.post(`${baseURL}/register`, user);
-    },
-    async getAll() {
-      this.allUsers = { loading: true };
-      try {
-        this.allUsers = await fetchWrapper.get(baseURL);
-      } catch (err) {
-        this.allUsers = { err };
-      }
-    },
-    async getById(id: any) {
-      this.user = { loading: true };
-      try {
-        this.user = await fetchWrapper.get(`${baseURL}/${id}`);
-      } catch (err) {
-        this.user = { err };
-      }
-    },
-    async update(id: any, params: any) {
-      await fetchWrapper.put(`${baseURL}/${id}`, params);
+export const useUserStore = defineStore("users",  () => {
+  const user = ref()
 
-      // if logged in user updates their profile, update user saved in localStorage
-      const authStore = useAuthStore();
-      if (id === authStore.user.id) {
-        const user = { ...authStore.user, ...params };
-        localStorage.setItem("user", JSON.stringify(user));
+  async function register(user:any) {
+    await fetchWrapper.post(`${baseURL}/register`, user);
+  }
+  async function getById(id:any) {
+    user.value = { loading: true };
+    try {
+      user.value = await fetchWrapper.get(`${baseURL}/${id}`);
+    } catch (err) {
+      user.value = { err };
+    }
+  }
 
-        authStore.user = user;
-      }
-    },
-    async updateItemCoin(id: any) {
-      await fetchWrapper.put(`${baseURL}/${id}`);
+  async function update(id:any, params: any) {
+    await fetchWrapper.put(`${baseURL}/${id}`, params);
+  
+    // if logged in user updates their profile, update user saved in localStorage
+    const authStore = useAuthStore();
+    if (id === authStore.user.id) {
+      const user = { ...authStore.user, ...params };
+      localStorage.setItem("user", JSON.stringify(user));
+  
+      authStore.user = user;
+    }
+  }
 
-      // if logged in user updates their profile, update user saved in localStorage
-      const authStore = useAuthStore();
-      if (id === authStore.user.id) {
-        const user = { ...authStore.user };
-        localStorage.setItem("user", JSON.stringify(user));
+  async function updateItemCoin(id:any) {
+    await fetchWrapper.put(`${baseURL}/${id}`);
+    // if logged in user updates their profile, update user saved in localStorage
+    const authStore = useAuthStore();
+    if (id === authStore.user.id) {
+      const user = { ...authStore.user };
+      localStorage.setItem("user", JSON.stringify(user));
+      authStore.user = user;
+    }
+  }
 
-        authStore.user = user;
-      }
-    },
-    async delete(id: any) {
-      // TODO
-      // for deleting a specific item
-      // TODO in BE as well!
-    },
-  },
+  return { user, register, getById, update, updateItemCoin }
 });
