9d8e1d8b1eb0965d5a5cd36c8d12fa46b1f40ff4
Marco <133157182+ProfessorClever@users.noreply.github.com>
2024-06-18 10:31:42 +0200

    merge

---
+++ frontend/src/views/game/Game.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SocketEvents from '../../components/SocketEvents.vue';
+
+</script>
+
+<template>
+  <SocketEvents msg="Connection Test" />
+
+</template>
+
+<style></style>
