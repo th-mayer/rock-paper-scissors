b03fb9f5ee45cfdf93efbc1607ea8967428d5c49
Marco <133157182+ProfessorClever@users.noreply.github.com>
2024-06-23 00:47:37 +0200

    Items Comps

---
+++ frontend/src/components/Item.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+    const props = defineProps({
+    itemKind: Number,
+    multiplier: Number
+    });
+</script>
+
+<template>
+    <div class="itemDiv" v-if="props.itemKind == 1"> <!-- More Damage with Stone -->
+        <img src="../../public/assets/rockdmg.png">
+        <div class="itemDiscription">
+            <h2>Heavy Stone</h2>
+            <p>Equipped person deals {{ props.multiplier }}x times the damage when winning with rock</p>
+        </div>
+    </div>
+    <div class="itemDiv" v-else-if="props.itemKind == 2"> <!-- More Damage with Paper -->
+        <img src="../../public/assets/paperdmg.png">
+        <div class="itemDiscription">
+            <h2>Sharp Paper</h2>
+            <p>Equipped person deals {{ props.multiplier }}x times the damage when winning with paper</p>
+        </div>
+    </div>
+    <div class="itemDiv" v-else-if="props.itemKind == 3"> <!-- More Damage with Scissors -->
+        <img src="../../public/assets/scissorsdmg.png">
+        <div class="itemDiscription">
+            <h2>Pointy Scissors</h2>
+            <p>Equipped person deals {{ props.multiplier }}x times the damage when winning with scissors</p>
+        </div>
+    </div>
+    <div class="itemDiv" v-else-if="props.itemKind == 4"> <!-- Protection from Stone -->
+        <img src="../../public/assets/rockdefense.png">
+        <div class="itemDiscription">
+            <h2>Brittle Stones</h2>
+            <p>Equipped person receives only {{ props.multiplier }}x times the damage when loosing aginst rock"</p>
+        </div>
+    </div>
+    <div class="itemDiv" v-else-if="props.itemKind == 5"> <!-- Protection from Paper -->
+        <img src="../../public/assets/paperdefense.png">
+        <div class="itemDiscription">
+            <h2>Soggy Paper</h2>
+            <p>Equipped person receives only {{ props.multiplier }}x times the damage when loosing aginst paper</p>
+        </div>
+    </div>
+    <div class="itemDiv" v-else-if="props.itemKind == 6"> <!-- Protection from Scissors -->
+        <img src="../../public/assets/scissorsdefense.png">
+        <div class="itemDiscription">
+            <h2>Blunt Scissors</h2>
+            <p>Equipped person receives only {{ props.multiplier }}x times the damage when loosing aginst scissors</p>
+        </div>
+    </div>
+    <div class="itemDiv" v-else> <!-- No Item -->
+        <img src="../../public/assets/empty.png">
+        <div class="itemDiscription">
+            <h2>No Item Equipped</h2>
+            <p>Win a match to be able to place an item here!</p>
+        </div>
+    </div> 
+</template>
+
+<style lang="scss">
+@import '@/css/main.scss';
+
+div.itemDiv {
+    width:100px;
+    height:100px;
+    border-radius: 20px;
+    border-width: 5px;
+    border-color: $bright-font-color;
+}
+
+div.itemDiscription {
+    visibility: hidden;
+    position: fixed;
+    width: 30vw;
+    background-color: $secondary-color;
+    padding: 2vh;
+    border-radius: 20px;
+    border-color: $backshadow;
+    border-width: 3px;
+    border: 3px;
+    border-style: solid;
+}
+
+div.itemDiscription > h2 {
+    font-size: 5vh;
+    margin: 0;
+    margin-bottom: 0.5vh;
+}
+
+div.itemDiscription > p {
+    font-size: 3vh;
+}
+
+div:hover > div.itemDiscription {
+    visibility: visible;
+}
+
+img {
+    width: 50px;
+    height: auto;
+}
+</style>
--- frontend/src/components/ItemPreview.vue
+++ frontend/src/components/ItemPreview.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
     import Card from './Card.vue';
+    import Item from './Item.vue';
 </script>
 
 <template>
     <Card title="Item manager" class="Homecard">
-        <div>
-            <img>
+        <div class="flex-row">
+            <Item :item-kind="1" :multiplier="2.2" />
+            <Item :item-kind="3" :multiplier="88.22" />
+            <Item :item-kind="5" :multiplier="0.2" />
         </div>
-        <div></div>
-        <div></div>
     </Card>
 </template>
