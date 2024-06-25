49e0aa9cd4ad02c17f46e1d84a9f6b2af7f4981c
th-mayer <theresa.mayer@hs-augsburg.de>
2024-06-25 13:30:52 +0200

    general modifier

--- frontend/src/helpers/randomItemGenerator.ts
+++ frontend/src/helpers/randomItemGenerator.ts
@@ -3,44 +3,38 @@ import { Item } from "../types/socket-connection-types";
 // TODO finetune modifier values
 export function generateRandomItem(): Item {
   let name, description: string;
-  let modifier: number;
+  // + converts string from toFixed back to a number
+  let modifier: number = +Math.random().toFixed(2);
   const kind = getRandomInt(6);
   switch (kind) {
     case 0:
       // 0 = dmg_r
       name = "Rock Damage";
-      // + converts string from toFixed back to a number
-      modifier = +Math.random().toFixed(2);
       description = `Grants extra damage when using Rock with a modifier of ${modifier}`;
       break;
     case 1:
       // 1 = dmg_p
       name = "Paper Damage";
-      modifier = +Math.random().toFixed(2);
       description = `Grants extra damage when using Paper with a modifier of ${modifier}`;
       break;
     case 2:
       // 2 = dmg_s
       name = "Scissors Damage";
-      modifier = +Math.random().toFixed(2);
       description = `Grants extra damage when using Scissors with a modifier of ${modifier}`;
       break;
     case 3:
       // 3 = prt_r
       name = "Rock Protection";
-      modifier = +Math.random().toFixed(2);
       description = `Grants extra protection against attacks using Rock with a modifier of ${modifier}`;
       break;
     case 4:
       // 4 = prt_p
       name = "Paper Protection";
-      modifier = +Math.random().toFixed(2);
       description = `Grants extra protection against attacks using Paper with a modifier of ${modifier}`;
       break;
     case 5:
       // 5 = prt_s
       name = "Scissors Protection";
-      modifier = +Math.random().toFixed(2);
       description = `Grants extra protection against attacks using Scissors with a modifier of ${modifier}`;
       break;
     default:
